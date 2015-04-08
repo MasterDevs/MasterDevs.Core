a040b4328ba0d0db0ccd2a482463534557dc57d1
Josh Quintus <josh@dotalign.com>
2015-04-08 19:44:39 -0400

    Adding Failing tests

---
+++ source/MasterDevs.Lib.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDevs.Lib.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
