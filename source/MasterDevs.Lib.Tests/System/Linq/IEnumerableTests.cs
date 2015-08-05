c73f16892516dc1e720f2677801c374b3e61164d
Josh <josh.quintus@gmail.com>
2015-08-04 21:38:12 -0400

    Add None extension

---
+++ source/MasterDevs.Lib.Tests/System/Linq/IEnumerableTests.cs
@@ -0,0 +1,80 @@
+﻿using MasterDevs.Lib.System.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MasterDevs.Lib.Tests.System.Linq
+{
+    [TestFixture]
+    public class IEnumerableTests
+    {
+        [Test]
+        public void None_EmptyEnumerable_ReturnsTrue()
+        {
+            // Assemble
+            IEnumerable<int> enumerable = Enumerable.Empty<int>();
+
+            // Act/Assert
+            Assert.IsTrue(enumerable.None());
+        }
+
+        [Test]
+        public void None_NonEmptyEnumerable_ReturnsFalse()
+        {
+            // Assemble
+            IEnumerable<int> enumerable = Enumerable.Range(0, 10);
+
+            // Act/Assert
+            Assert.IsFalse(enumerable.None());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void None_NullEnumerable_Throws()
+        {
+            // Assemble
+            IEnumerable<string> enumerable = null;
+
+            // Act
+            enumerable.None();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void None_NullPredicate_Throws()
+        {
+            // Assemble
+            IEnumerable<int> enumerable = Enumerable.Range(0, 10);
+
+            // Act
+            var result = enumerable.None(null);
+        }
+
+        [Test]
+        public void None_PredicateAlwaysReturnsFalse_ReturnsTrue()
+        {
+            // Assemble
+            IEnumerable<int> enumerable = Enumerable.Range(0, 10);
+
+            // Act
+            var result = enumerable.None(_ => false);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void None_PredicateAlwaysReturnsTrue_ReturnsFalse()
+        {
+            // Assemble
+            IEnumerable<int> enumerable = Enumerable.Range(0, 10);
+
+            // Act
+            var result = enumerable.None(_ => true);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
