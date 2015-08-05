e5c0d8d798aef83cd3b93e13fa0932a4d9375cda
Josh <josh.quintus@gmail.com>
2015-08-04 21:01:57 -0400

    Adding code contracts

---
+++ source/MasterDevs.Lib/CodeContracts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MasterDevs.Lib
+{
+    public static class CodeContracts
+    {
+        public static T RequireNotNull<T>(this T me, string argumentName = "") where T : class
+        {
+            if (me == null) throw new ArgumentNullException(argumentName);
+            return me;
+        }
+
+        public static T RequireHasValue<T>(this T? me, string argumentName = "") where T : struct
+        {
+            if (me == null || !me.HasValue) throw new ArgumentNullException(argumentName);
+            return me.Value;
+        }
+
+        public static string RequireNotNullOrEmpty(this string me, string argumentName = "")
+        {
+            if (string.IsNullOrEmpty(me)) throw new ArgumentNullException(argumentName);
+            return me;
+        }
+
+        public static string RequireNotNullOrWhiteSpace(this string me, string argumentName = "")
+        {
+            if (string.IsNullOrWhiteSpace(me)) throw new ArgumentNullException(argumentName);
+            return me;
+        }
+    }
+}
