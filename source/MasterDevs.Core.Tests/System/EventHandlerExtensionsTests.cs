a5973be427ede0de8408d6f71264205c35cbe3eb
Josh <josh.quintus@gmail.com>
2015-08-20 07:24:07 -0400

    Add tests for the event handler extensions.

---
+++ source/MasterDevs.Core.Tests/System/EventHandlerExtensionsTests.cs
@@ -0,0 +1,161 @@
+﻿using MasterDevs.Core.Common.Infrastructure;
+using MasterDevs.Core.System;
+using NUnit.Framework;
+using System;
+
+namespace MasterDevs.Core.Tests.System
+{
+    [TestFixture]
+    public class EventHandlerExtensionsTests
+    {
+        private EventArgs _eventArgs = new EventArgs();
+        private EventArgs<string> _genericEventArgs = new EventArgs<string>("helo world");
+        private object _sender = new object();
+
+        [Test]
+        public void SafeInvokeNoEventArgs_NonNullEventHandler_RaisesEventWithEmptyArgs()
+        {
+            // Assemble
+            bool ran = false;
+            Eventy eventy = new Eventy();
+            eventy.Handler += (s, e) =>
+            {
+                ran = true;
+                Assert.AreEqual(_sender, s);
+                Assert.AreEqual(EventArgs.Empty, e);
+            };
+
+            // Act
+            eventy.Raise(_sender);
+
+            // Assert
+            Assert.IsTrue(ran);
+        }
+
+        [Test]
+        public void SafeInvokeNoEventArgs_NullEventHandler_DoesNothing()
+        {
+            // Act
+            EventHandlerExtensions.SafeInvoke(null, _sender);
+        }
+
+        [Test]
+        public void SafeInvokeWithEventArgs_ArgsNull_RaisesEventWithNullArgs()
+        {
+            // Assemble
+            bool ran = false;
+            var eventy = new Eventy();
+            eventy.Handler += (s, e) =>
+            {
+                ran = true;
+                Assert.AreEqual(_sender, s);
+                Assert.IsNull(e);
+            };
+
+            // Act
+            eventy.Raise(_sender, null);
+
+            // Assert
+            Assert.IsTrue(ran);
+        }
+
+        [Test]
+        public void SafeInvokeWithEventArgs_ArgsSpecified_RaisesEventWithArgs()
+        {
+            // Assemble
+            bool ran = false;
+            var eventy = new Eventy();
+            eventy.Handler += (s, e) =>
+            {
+                ran = true;
+                Assert.AreEqual(_sender, s);
+                Assert.AreEqual(_eventArgs, e);
+            };
+
+            // Act
+            eventy.Raise(_sender, _eventArgs);
+
+            // Assert
+            Assert.IsTrue(ran);
+        }
+
+        [Test]
+        public void SafeInvokeWithEventArgs_NullEventHandler_DoesNothing()
+        {
+            // Act
+            EventHandlerExtensions.SafeInvoke((EventHandler)null, _sender, _eventArgs);
+        }
+
+        [Test]
+        public void SafeInvokeWithGenericArgs_ArgsNull_RaisesEventWithNullArgs()
+        {
+            // Assemble
+            bool ran = false;
+            var eventy = new GenericEventy();
+            eventy.Handler += (s, e) =>
+            {
+                ran = true;
+                Assert.AreEqual(_sender, s);
+                Assert.IsNull(e);
+            };
+
+            // Act
+            eventy.Raise(_sender, null);
+
+            // Assert
+            Assert.IsTrue(ran);
+        }
+
+        [Test]
+        public void SafeInvokeWithGenericArgs_ArgsSpecified_RaisesEventWithArgs()
+        {
+            // Assemble
+            bool ran = false;
+            var eventy = new GenericEventy();
+            eventy.Handler += (s, e) =>
+            {
+                ran = true;
+                Assert.AreEqual(_sender, s);
+                Assert.AreEqual(_genericEventArgs.Value, e);
+            };
+
+            // Act
+            eventy.Raise(_sender, _genericEventArgs);
+
+            // Assert
+            Assert.IsTrue(ran);
+        }
+
+        [Test]
+        public void SafeInvokeWithGenericArgs_NullEventHandler_DoesNothing()
+        {
+            // Act
+            EventHandlerExtensions.SafeInvoke(null, _sender, _genericEventArgs);
+        }
+
+        public class Eventy
+        {
+            public event EventHandler Handler;
+
+            public void Raise(object sender)
+            {
+                Handler.SafeInvoke(sender);
+            }
+
+            public void Raise(object sender, EventArgs args)
+            {
+                Handler.SafeInvoke(sender, args);
+            }
+        }
+
+        public class GenericEventy
+        {
+            public event EventHandler<string> Handler;
+
+            public void Raise(object sender, string args)
+            {
+                Handler.SafeInvoke(sender, args);
+            }
+        }
+    }
+}
--- source/MasterDevs.Core/System/EventHandlerExtensions.cs
+++ source/MasterDevs.Core/System/EventHandlerExtensions.cs
@@ -1,53 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MasterDevs.Core.System
 {
- public static   class EventHandlerExtensions
+    public static class EventHandlerExtensions
     {
-
-
-
-
         public static void SafeInvoke(this EventHandler ev, object sender)
         {
             ev.SafeInvoke(sender, EventArgs.Empty);
         }
 
-
-
-
-
-
-
-
-
-
         public static void SafeInvoke(this EventHandler ev, object sender, EventArgs e)
         {
-            if (null == ev)
-                return;
+            if (null == ev) return;
 
             ev(sender, e);
         }
 
         public static void SafeInvoke<T>(this EventHandler<T> ev, object sender, T e)
         {
-            if (null == ev)
-                return;
+            if (null == ev) return;
 
             ev(sender, e);
         }
-
-
-
-
-
-
-
-
     }
 }
